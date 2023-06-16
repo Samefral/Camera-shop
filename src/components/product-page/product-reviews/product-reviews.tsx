baee211782de61a1923c9279d910a8628cef19d8
Samefral <galchun174@mail.ru>
2023-06-16 21:21:45 +0500

    implemented the rendering of reviews by scrolling the page

--- src/components/product-page/product-reviews/product-reviews.tsx
+++ src/components/product-page/product-reviews/product-reviews.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useAppSelector } from '../../../hooks';
 import { getReviews, getReviewsDataLoadingStatus } from '../../../store/reviews-data/selectors';
 import { Reviews } from '../../../types/review';
@@ -9,11 +9,44 @@ const DEFAULT_RENDERED_REVIEWS_QUANTITY = 3;
 const REVIEWS_TO_RENDER_QUANTITY = 3;
 
 const sortReviewsByDate = (reviews: Reviews) => [...reviews].sort((a, b) => Date.parse(b.createAt) - Date.parse(a.createAt));
+const checkIfReachedEndOfPage = () => window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight;
+const areAllReviewsDisplayed = (renderedReviews: number, reviews: Reviews) => renderedReviews >= reviews.length;
 
 function ProductReviews(): JSX.Element {
   const [renderedReviews, setRenderedReviews] = useState(DEFAULT_RENDERED_REVIEWS_QUANTITY);
+  const [hasScrolledToBottom, setHasScrolledToBottom] = useState(false);
   const reviews = sortReviewsByDate(useAppSelector(getReviews));
   const isReviewsLoading = useAppSelector(getReviewsDataLoadingStatus);
+
+  const handleScroll = () => {
+    if (checkIfReachedEndOfPage()) {
+      setHasScrolledToBottom(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    if (areAllReviewsDisplayed(renderedReviews, reviews)) {
+      window.removeEventListener('scroll', handleScroll);
+    }
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [renderedReviews, reviews]);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    if (isMounted && hasScrolledToBottom && !areAllReviewsDisplayed(renderedReviews, reviews)) {
+      setRenderedReviews(renderedReviews + REVIEWS_TO_RENDER_QUANTITY);
+      setHasScrolledToBottom(false);
+    }
+
+    return () => {
+      isMounted = false;
+    };
+
+  }, [hasScrolledToBottom, renderedReviews, reviews]);
 
   if (isReviewsLoading) {
     return <LoadingScreen />;
@@ -34,7 +67,7 @@ function ProductReviews(): JSX.Element {
           </ul>
           <div className="review-block__buttons">
             {
-              renderedReviews >= reviews.length ? null :
+              areAllReviewsDisplayed(renderedReviews, reviews) ? null :
                 <button
                   onClick={() => setRenderedReviews(renderedReviews + REVIEWS_TO_RENDER_QUANTITY)}
                   className="btn btn--purple"
